9ce13ed6fed67e51e43a294d400ad7f3681b91fc
spy <1285230617@qq.com>
2026-01-23 17:39:30 +0800

    学习总结

--- docs/Java/code/src/main/java/com/abc/Class/test_1.java
+++
@@ -1,8 +0,0 @@
-package com.abc.Class;
-
-public class test_1 {
-
-    public static void main(String[] args) {
-
-    }
-}
--- docs/Java/code/src/main/java/com/abc/reflection/User.java
+++
@@ -1,6 +0,0 @@
-package com.abc.reflection;
-
-class User {
-    private String name = "Hidden";
-    public void sayHi() { System.out.println("Hi!"); }
-}
